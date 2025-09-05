618b1eb239f69f51d9f98b1abf8eefdb52f8042a
Jogis <ronipihajoki@proton.me>
2025-09-05 21:57:41 +0300

    KOHTA 5 TIIMI
    Tein barit burritot bissen kylkee

--- Kayttajasyoteharjoitus/src/App.java
+++ Kayttajasyoteharjoitus/src/App.java
@@ -6,17 +6,28 @@ public class App {
 
         int n1 = 0;
         int n2 = 0;
+        String lasku = "";
+
 
         System.out.println("Anna mulle yksi numero");
             n1 = Integer.parseInt(in.nextLine());        
         System.out.println("Anna mulle toine numero");
             n2 = Integer.parseInt(in.nextLine());
-        int vastaus = n1 + n2;
-        if (vastaus > 10){
-        System.out.println("tulos yli 10. JOka on " + vastaus);
+        System.out.println("Haluatko plus vai miinus");
+            lasku = in.nextLine();
+        if (lasku.equals("plus")){
+            int vastaus = n1 + n2;
+            System.out.println("Summa on " + vastaus);
         }
-        else{
-            System.out.println("Tulos on " + vastaus);
+        else {
+            int vastaus = n1 - n2;
+            System.out.println("Tulo on " + vastaus);
         }
+        // if (vastaus > 10){
+        // System.out.println("tulos yli 10. JOka on " + vastaus);
+        // }
+        // else{
+        //     System.out.println("Tulos on " + vastaus);
+        // }
     }   
 }
