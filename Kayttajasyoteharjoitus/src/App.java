01ec815841d8ceaedfea7f0a20027ff309d4bb61
Jogis <ronipihajoki@proton.me>
2025-09-05 21:24:48 +0300

    Kohta 4 kylla toimii hjyvin
    Kalja counte = 2

--- Kayttajasyoteharjoitus/src/App.java
+++ Kayttajasyoteharjoitus/src/App.java
@@ -12,7 +12,11 @@ public class App {
         System.out.println("Anna mulle toine numero");
             n2 = Integer.parseInt(in.nextLine());
         int vastaus = n1 + n2;
-        System.out.println("Lukujen summa " + vastaus);
-        ;
+        if (vastaus > 10){
+        System.out.println("tulos yli 10. JOka on " + vastaus);
+        }
+        else{
+            System.out.println("Tulos on " + vastaus);
+        }
     }   
 }
