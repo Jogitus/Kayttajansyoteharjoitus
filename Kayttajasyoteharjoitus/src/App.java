195a75365d7a6a4ec22334760bc4c2605c26d0d8
Jogis <ronipihajoki@proton.me>
2025-09-05 21:10:07 +0300

    Koodin Pohja

--- Kayttajasyoteharjoitus/src/App.java
+++ Kayttajasyoteharjoitus/src/App.java
@@ -1,5 +1,17 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
+        Scanner in = new Scanner(System.in);
+
+        int n1 = 0;
+        int n2 = 0;
+
+        System.out.println("Anna mulle yksi numero");
+            n1 = Integer.parseInt(in.nextLine());        
+        System.out.println("Anna mulle toine numero");
+            n2 = Integer.parseInt(in.nextLine());
+        System.out.println("Eka numerosi on " + n1);
+        System.out.println("Ja toka kummone ois " + n2);
+    }   
 }
